bb9db7e38c2f00b833d14c71b9125c27cba0fa65
FMFSc Gandalf <fellipescirea@gmail.com>
2024-02-12 21:27:11 -0800

    Capstone Project Initialization - Oracle University

---
+++ src/humanresourcesprogram/HumanResourcesProgram.java
@@ -0,0 +1,16 @@
+package humanresourcesprogram;
+
+/**
+ *
+ * @author fellipescirea
+ */
+public class HumanResourcesProgram {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
