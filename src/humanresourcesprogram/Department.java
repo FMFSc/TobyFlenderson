65376fb952b6b40a9175a9217b20afe188fd9e1c
FMFSc Gandalf <fellipescirea@gmail.com>
2024-02-12 21:47:51 -0800

    First commit with classes structure

---
+++ src/humanresourcesprogram/Department.java
@@ -0,0 +1,11 @@
+package humanresourcesprogram;
+
+/**
+ *
+ * @author fellipescirea
+ */
+public class Department 
+{
+    public String deptName;
+    
+}
---
+++ src/humanresourcesprogram/Employee.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package humanresourcesprogram;
+
+/**
+ *
+ * @author fellipescirea
+ */
+public class Employee 
+{
+    public int ID;
+    public String employeeName;
+    public double salary;
+    
+        
+}
